33a3f522caf9ea57f018c01ac0e023e65b251483
Sasha Tlr <sashatailor@gmail.com>
2017-02-11 11:32:56 -0500

    Add user model

--- thnq/config/routes.rb
+++ thnq/config/routes.rb
@@ -1,49 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'questions/index'
+  resources :journals, :entries, :questions, :users
 
-  get 'questions/create'
-
-  get 'questions/new'
-
-  get 'questions/edit'
-
-  get 'questions/show'
-
-  get 'questions/update'
-
-  get 'questions/destroy'
-
-  get 'entries/index'
-
-  get 'entries/create'
-
-  get 'entries/new'
-
-  get 'entries/edit'
-
-  get 'entries/show'
-
-  get 'entries/update'
-
-  get 'entries/destroy'
-
-  get 'journals/index'
-
-  get 'journals/create'
-
-  get 'journals/new'
-
-  get 'journals/edit'
-
-  get 'journals/show'
-
-  get 'journals/update'
-
-  get 'journals/destroy'
-
-  resources :journals
-  get 'user/index'
 
   root 'user#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
